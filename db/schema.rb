f43e7c7f45debbba2dc5aa1320382de4eff61318
Igor Rocha <igor@inventosdigitais.com.br>
2018-02-06 01:34:20 -0200

    generated Offer model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180205035702) do
+ActiveRecord::Schema.define(version: 20180206032831) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,19 @@ ActiveRecord::Schema.define(version: 20180205035702) do
     t.datetime "updated_at", null: false
     t.datetime "deleted_at"
     t.index ["email"], name: "index_admins_on_email", unique: true
+  end
+
+  create_table "offers", force: :cascade do |t|
+    t.string "advertiser_name"
+    t.string "url"
+    t.text "description"
+    t.datetime "starts_at"
+    t.datetime "ends_at"
+    t.boolean "premium"
+    t.integer "status"
+    t.datetime "deleted_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
